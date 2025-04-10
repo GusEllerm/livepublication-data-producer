c347faf2dd57c5968d862375de374075c050faea
Augustus Ellerm <ellermaugustus@gmail.com>
2025-04-11 09:36:53 +1200

    Fix import storting

--- Scripts/utils/job_utils.py
+++ Scripts/utils/job_utils.py
@@ -3,7 +3,7 @@ import shutil
 import zipfile
 from datetime import datetime
 
-from utils.logging_utils import log_block, log_step, log_error
+from utils.logging_utils import log_block, log_error, log_step
 
 
 def generate_job_id(
